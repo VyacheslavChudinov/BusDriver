8784f29ca7ecad6ba33246f23c50c4a626c30094
Viacheslav Chudinov <8038118+VyacheslavChudinov@users.noreply.github.com>
2024-03-06 21:11:44 +0300

    Initial

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 30f;
+    public float turnSpeed = 50f;
+    private float horizontalInput;
+    private float forwardInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        if (transform.position.y < -5)
+        {
+            transform.position = new Vector3(0, 0, 0);
+            transform.forward = new Vector3(0, 0, 0);
+            transform.up = new Vector3(0, 0, 0);
+        } 
+        else
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+            transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+        }
+
+        
+
+        
+    }
+}
